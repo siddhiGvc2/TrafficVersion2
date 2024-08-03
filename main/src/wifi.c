c53c8c6b11d748ed3c7da63679e59459dac1f9fc
GVC Systems <documents@gvc.in>
2024-08-03 13:19:34 +0530

    ESP Touch when jumper removed

--- main/src/wifi.c
+++ main/src/wifi.c
@@ -93,12 +93,26 @@ void event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
 {
     char buffer[100];
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) 
+    {
          if (gpio_get_level(JUMPER2) == 0)
          {
-             xTaskCreate(smartconfig_example_task, "smartconfig_example_task", 4096, NULL, 6, NULL);
+            ESP_LOGI(TAG,"*Waiting for jumper to be removed#");
+            uart_write_string_ln("*Waiting for jumper to be removed#");
+            while (1)
+            {
+                while (gpio_get_level(JUMPER2) == 0)
+                    vTaskDelay(100);   
+                if (gpio_get_level(JUMPER2) == 0)
+                    continue;
+                else    
+                    break;
+            }
+            
+            xTaskCreate(smartconfig_example_task, "smartconfig_example_task", 4096, NULL, 6, NULL);
             set_led_state(SEARCH_FOR_ESPTOUCH);
             ESP_LOGI(TAG,"*Start Looking for ESP TOUCH#");
+            uart_write_string_ln("*Start Looking for ESP TOUCH#");
          }
          else
          {
