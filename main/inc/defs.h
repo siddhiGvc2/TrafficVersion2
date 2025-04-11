fea0c1bc7666c584e3fa58b8a3e527348d06f040
siddhiGvc2 <sd@gvc.in>
2025-04-11 14:45:38 +0530

    version 1.22C

--- main/inc/defs.h
+++ main/inc/defs.h
@@ -160,7 +160,7 @@ typedef enum TCPIP_SOCKET_STATE{
 
 #define PASS_THRU   "Y"
 
-#define FWVersion "*Kwikpay_080425_VER_1.22B Naico Ltd#"
+#define FWVersion "*Kwikpay_110425_VER_1.22C Naico Ltd#"
 
 
 
